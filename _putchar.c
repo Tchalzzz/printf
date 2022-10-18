f3b37ada671a2615a337d0be457fbdc3fb6357f8
Effiong Ekpe <effiongekpe02@gmail.com>
2022-10-18 15:10:43 -0700

    Printf Team Task

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
-// _putchar - function to return character to stdout
+/**
+ * _putchar - function to return character to stdout
+ * @c: Character to be printed
+ * Return: character
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
